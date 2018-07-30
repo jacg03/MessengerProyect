abd14a4aad7ba9540118ced6fc5b62b07b3ffece
jacg03 <03jacg@gmail.com>
2018-07-30 12:42:51 -0500

    adding container component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HistoryComponent } from './history/history.component';
 import { InboxComponent } from './inbox/inbox.component';
 import { DetailComponent } from './detail/detail.component';
+import { ContainerComponent } from './container/container.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HistoryComponent,
     InboxComponent,
-    DetailComponent
+    DetailComponent,
+    ContainerComponent
   ],
   imports: [
     BrowserModule,
