98e09b6957333bac09dd320a7d095923217e6b1e
jacg03 <03jacg@gmail.com>
2018-08-02 11:15:11 -0500

    message array structure

--- src/app/services/message.service.ts
+++ src/app/services/message.service.ts
@@ -4,6 +4,20 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class MessageService {
+  private messages:any = [
+    {
+      id: 1,
+      text: "Hola",
+      isIncoming: true,
+      chatId: 1
+    },
+    {
+      id: 2, 
+      text: "Que tal",
+      isIncoming: false,
+      chatId: 1
+    }
+  ];
 
   constructor() { }
 }
